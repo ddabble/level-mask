dbe6d292330e7c0fe98466788fba7c3419163c4e
Anders <6058745+ddabble@users.noreply.github.com>
2026-01-31 14:05:06 +0100

    Implemented Mask interface for RevealMask

--- Assets/Scripts/Masks/RevealMask.cs
+++ Assets/Scripts/Masks/RevealMask.cs
@@ -1,19 +1,18 @@
 using UnityEngine;
-using UnityEngine.UI;
 
-public class RevealMask : MonoBehaviour
+public class RevealMask : MonoBehaviour, Mask
 {
     [SerializeField]
     private LayerMask affectedObjectLayer;
 
-    void OnEnable()
+    public void OnEquipped()
     {
         foreach (var item in FindObjectsByType<GameObject>(sortMode: FindObjectsSortMode.None))
             if ((affectedObjectLayer.value & (1 << item.layer)) != 0)
                 item.SetActive(false);
     }
 
-    private void OnDisable()
+    public void OnUnequipped()
     {
         foreach (var item in FindObjectsByType<GameObject>(FindObjectsInactive.Include, FindObjectsSortMode.None))
             if ((affectedObjectLayer.value & (1 << item.layer)) != 0)
